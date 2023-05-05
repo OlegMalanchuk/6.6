fe8e2a32462b7b0538a74941ca65724cd96a54d7
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-05 10:19:29 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        RectangularPrism rectangularPrism = new RectangularPrism();
+        RectangularPyramid rectangularPyramid = new RectangularPyramid();
+        TriangularPrism triangularPrism = new TriangularPrism();
+
+
+
+
+
+
+
+
+
+    }
+}
